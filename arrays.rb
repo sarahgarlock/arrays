218c645e226ba04f4c2a91e98f960d451586110c
Sarah Garlock <garlock.sarah10@gmail.com>
2023-02-07 21:17:53 -0700

    Edit comments

--- arrays.rb
+++ arrays.rb
@@ -5,12 +5,19 @@ booleans = [hungry = true, tired = true, sore = true, unmotivated = false]
 
 # .pop removes and returns the end element
 puts names.pop
+# returns "Jamal"
 
 # .push add aruments to end of element list
 puts numbers.push(1000)
+# returns 1000 to the end of the element list
 
 # .shift removes and returns leading element
 puts floats.shift
+# returns 2.1
 
 # .unshift adds objects to beginning of element list
 puts booleans.unshift(unfunny = false)
+# returns false to beginnig of element list
+
+#Open-ended
+
