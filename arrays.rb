ab494a982a04e4601bf39afa185a7f1dea1e4cd0
Sarah Garlock <garlock.sarah10@gmail.com>
2023-02-07 21:02:48 -0700

    Declare variables

--- arrays.rb
+++ arrays.rb
@@ -1,1 +1,4 @@
-Ruby
+names = ["Adam", "Davis", "Allison", "Jamal"]
+numbers = [1, 10, 33, 19]
+floats = [2.1, 3.5, 42.24, 93.733]
+booleans = [hungry = true, tired = true, sore = true, unmotivated = false]
