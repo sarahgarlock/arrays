bfaf79e5f0b196e5482bbc07f6f9cd2db0cbcd57
Sarah Garlock <garlock.sarah10@gmail.com>
2023-02-07 21:12:56 -0700

    Add explanations to calls

--- arrays.rb
+++ arrays.rb
@@ -2,3 +2,15 @@ names = ["Adam", "Davis", "Allison", "Jamal"]
 numbers = [1, 10, 33, 19]
 floats = [2.1, 3.5, 42.24, 93.733]
 booleans = [hungry = true, tired = true, sore = true, unmotivated = false]
+
+# .pop removes and returns the end element
+puts names.pop
+
+# .push add aruments to end of element list
+puts numbers.push(1000)
+
+# .shift removes and returns leading element
+puts floats.shift
+
+# .unshift adds objects to beginning of element list
+puts booleans.unshift(unfunny = false)
